ae22f65cb562db3d204eb776e86a385b9e6f5f64
Glenn Ferrie <glenn.ferrie@gmail.com>
2013-04-04 00:16:30 -0400

    base site, with two areas

---
+++ ExtraspaceApp/ExtraspaceApp/Areas/m/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtraspaceApp.Areas.m.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /m/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ExtraspaceApp/ExtraspaceApp/Areas/m/mAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace ExtraspaceApp.Areas.m
+{
+    public class mAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "m";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "m_default",
+                "m/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "ExtraspaceApp.Areas.m.Controllers" }
+            );
+
+        }
+    }
+}
